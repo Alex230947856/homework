aae382539ee7ce1ac9f3c3ef9f025bde066f2e4f
Alex230947856 <80635108+Alex230947856@users.noreply.github.com>
2021-03-14 20:18:52 +0500

    Задание на 13-03-2021

---
+++ EuclidGeometry/EuclidGeometry/Geometry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclidGeometry
+{
+    public static class Geometry
+    {
+        public static Segment CreateSegment(Point a, Point b)
+        {
+            var segment = new Segment(a, b);
+
+            return segment;
+        }
+        public static Triangle CreateTriangle(Point a, Point b, Point c, Segment ab, Segment bc, Segment ac)
+        {
+            ab = new Segment(a, b);
+            bc = new Segment(b, c);
+            ac = new Segment(a, c);
+            var triangle = new Triangle(a, b, c, ab, bc, ac);
+
+            if ((a.IsInsideSegment(triangle.segmentBC) || b.IsInsideSegment(triangle.segmentAC) || c.IsInsideSegment(triangle.segmentAC)) == false)
+                return triangle;
+            else
+                throw new ArgumentException("Triangle is incorrect");
+        }
+    }
+}
---
+++ EuclidGeometry/EuclidGeometry/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclidGeometry
+{
+    public class Triangle
+    {
+        public Point A;
+        public Point B;
+        public Point C;
+        public Segment segmentAB;
+        public Segment segmentBC;
+        public Segment segmentAC;
+        
+
+        public Triangle(Point a, Point b, Point c, Segment ab, Segment bc, Segment ac)
+        {
+            A = a;
+            B = b;
+            C = c;
+
+            ab = new Segment(a, b);
+            bc = new Segment(b, c);
+            ac = new Segment(a, c);
+            segmentAB = ab;
+            segmentBC = bc;
+            segmentAC = ac;
+            
+        }
+        public double Area
+        {
+            get
+            {
+                var semiPerimeter = (segmentAB.Length+segmentBC.Length+segmentAC.Length)/2;
+                return Math.Sqrt(semiPerimeter*(semiPerimeter-segmentAB.Length)*(semiPerimeter-segmentBC.Length)*(semiPerimeter-segmentAC.Length));
+            }
+        }
+    }
+}
